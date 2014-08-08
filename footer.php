239c012597df0a67a3f11a10a7dc0681b73904b6
Chris Wallace <chris@liftux.com>
2014-08-08 09:07:58 -0500

    Added base footer styles.

--- footer.php
+++ footer.php
@@ -11,11 +11,13 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'shepherd' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'shepherd' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'shepherd' ), 'shepherd', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
-		</div><!-- .site-info -->
+		<div class="site-footer-inner">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'shepherd' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'shepherd' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'shepherd' ), 'Shepherd Pro', '<a href="https://upthemes.com/" rel="designer">UpThemes</a>' ); ?>
+			</div><!-- .site-info -->
+		</div><!-- .site-footer-inner -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
