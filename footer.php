a34c6577d1fbdc507d2c2c010e5d7348a2acc254
Anthony Garand <anthonygarand@gmail.com>
2014-10-08 16:08:15 -0400

    Add support for 3 footer widget areas

--- footer.php
+++ footer.php
@@ -12,6 +12,17 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-footer-inner">
+			<div class="footer-widget-area">
+				<div class="footer-widget">
+					<?php dynamic_sidebar( 'footer-1' ); ?>
+				</div>
+				<div class="footer-widget">
+					<?php dynamic_sidebar( 'footer-2' ); ?>
+				</div>
+				<div class="footer-widget">
+					<?php dynamic_sidebar( 'footer-3' ); ?>
+				</div>
+			</div>
 			<div class="site-info">
 				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'shepherd' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'shepherd' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
--- functions.php
+++ functions.php
@@ -116,6 +116,36 @@ function shepherd_widgets_init() {
 		'before_title'  => '<h6 class="widget-title">',
 		'after_title'   => '</h6>',
 	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Footer Left', 'shepherd' ),
+		'id'            => 'footer-1',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h6 class="widget-title">',
+		'after_title'   => '</h6>',
+	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Footer Center', 'shepherd' ),
+		'id'            => 'footer-2',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h6 class="widget-title">',
+		'after_title'   => '</h6>',
+	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Footer Right', 'shepherd' ),
+		'id'            => 'footer-3',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h6 class="widget-title">',
+		'after_title'   => '</h6>',
+	) );
 }
 add_action( 'widgets_init', 'shepherd_widgets_init' );
 
